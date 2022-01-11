f428d10bb6a5c6aea59c6ff8f6292f1fefb3e8d4
Florian Görlich <florian.goerlich95@gmail.com>
2022-01-11 14:53:11 +0100

    created random color picker

---
+++ index.js
@@ -0,0 +1,21 @@
+import randomColor from 'randomcolor'; // import the script
+import chalk from 'chalk';
+const color = randomColor().toUpperCase(); // a hex code for an attractive color: ;
+
+const colorInput = process.argv[2];
+const lumInput = process.argv[3];
+
+if (!process.argv[2]) {
+  console.log(chalk.hex(`${randomColor()}`)(color));
+} else if (process.argv[2] && process.argv[3]) {
+  console.log(
+    chalk.hex(
+      `${randomColor({
+        luminosity: lumInput,
+        hue: colorInput,
+      })}`,
+    )(color),
+  );
+} else if (process.argv[2] === 'ask') {
+  console.log(chalk.red('Type in a color and luminosity'.toUpperCase()));
+}
